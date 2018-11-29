bd974c5cc40c68c97924c012ec1931e1f999cd34
Dejan Trajkovic <dtrajko@gmail.com>
2018-11-29 03:53:57 +0100

    S04 L171 CrossProduct() Vector Operator (take 2)

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -50,7 +50,7 @@ public:
 		TSubclassOf<AProjectile> ProjectileBlueprint = nullptr;
 
 	UPROPERTY(EditDefaultsOnly, Category = Firing)
-		float LaunchSpeed = 2000.0f; // Sensible starting value of 1000 m/s
+		float LaunchSpeed = 3000.0f; // Sensible starting value of 1000 m/s
 
 	UPROPERTY(EditDefaultsOnly, Category = Firing)
 		float ReloadTimeInSeconds = 5;
